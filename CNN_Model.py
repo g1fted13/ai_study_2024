1251b3cdc137edb70787db39a57a96b5f9afd727
Woohyun Song <1000balls@gmail.com>
2024-01-18 05:20:26 +0900

    세 번째 모델

--- CNN_Model.py
+++ CNN_Model.py
@@ -47,16 +47,16 @@ model = Sequential()
 
 #첫 번째 Conv층 쌓기
 #Input : 32 * 32 * 3 image
-#Filters: (3 * 3 * 3) * 16 -> 16 filters
-#Output: 32 * 32 * 16
+#Filters: (3 * 3 * 3) * 16 -> 32 filters
+#Output: 32 * 32 * 32
 
 model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=3, padding='same', activation='relu', input_shape=(32, 32, 3)))
+model.add(Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(32, 32, 3)))
 
 
 #두 번째 Conv층 쌓기
-#Input: 32 * 32 * 16
-#Filters: (3 * 3 * 16) * 32 -> 32 filters
+#Input: 32 * 32 * 32
+#Filters: (3 * 3 * 32) * 32 -> 32 filters
 #Output: 32 * 32 * 32
 #MaxPool: 16 * 16 * 32
 
@@ -68,41 +68,60 @@ model.add(MaxPool2D(pool_size=2))
 #Input: 16 * 16 * 32
 #Filters: (3 * 3 * 32) * 64 -> 64 filters
 #Output: 16 * 16 * 64
+
+model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
+
+#네 번째 Conv층 쌓기
+#Input: 16 * 16 * 64
+#Filters: (3 * 3 * 64) * 64 -> 64 filters
+#Output: 16 * 16 * 64
 #MaxPool: 8 * 8 * 64
 
 model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPool2D(pool_size=2))
 
-#네 번째 Conv층 쌓기
+#다섯 번째 Conv층 쌓기
 #Input: 8 * 8 * 64
 #Filters: (3 * 3 * 64) * 128 -> 128 filters
 #Output: 8 * 8 * 128
 
 model.add(Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'))
 
-#다섯 번째 Conv층 쌓기
+#여섯 번째 Conv층 쌓기
 #Input: 8 * 8 * 128
-#Filters: (3 * 3 * 128) * 64 -> 64 filters
-#Output: 8 * 8 * 64
-#MaxPool: 4 * 4 * 64
+#Filters: (3 * 3 * 128) * 128 -> 128 filters
+#Output: 8 * 8 * 128
 
-model.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'))
+model.add(Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'))
+
+#일곱 번째 Conv층 쌓기
+#Input: 8 * 8 * 128
+#Filters: (3 * 3 * 128) * 128 -> 128 filters
+#Output: 8 * 8 * 128
+#MaxPool: 4 * 4 * 128
+
+model.add(Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPool2D(pool_size=2))
 
 #Dropout 층 쌓기
-#훈련 시 Layer의 30%의 뉴런을 무작위로 비활성화
-model.add(Dropout(0.3))
+#훈련 시 Layer의 20%의 뉴런을 무작위로 비활성화
+model.add(Dropout(0.2))
 
 
 #Flatten
-#Input: 4 * 4 * 64
-#Output: 1024
+#Input: 4 * 4 * 128
+#Output: 2028
 model.add(Flatten())
 
 
-#One Fully-Connected Layer
-#Input: 1024
-#Output: 10
+#Two Fully-Connected Layer
+#Input: 2028
+#Intermediate Output: 500
+#Dropout
+#40%의 뉴런을 무작위로 비활성화
+#Final Output: 10
+model.add(Dense(500, activation='relu'))
+model.add(Dropout(0.4))
 model.add(Dense(10, activation='softmax'))
 
 #모델 요약
@@ -113,6 +132,10 @@ model.summary()
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
 
+# Data Augmentation 설정 (좌우반전)
+from keras.preprocessing.image import ImageDataGenerator
+datagen = ImageDataGenerator(horizontal_flip=True)
+
 #모델 학습하기
 #callback의 진행상황에 대한 상세 출력 활성화; 각 에포크마다 결과가 출력됨
 #전체 훈련 데이터에 대해서 10번 에포크 반복
@@ -120,7 +143,7 @@ model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['ac
 from keras.callbacks import ModelCheckpoint
 checkpointer = ModelCheckpoint(filepath='model.weights.best.hdf5', verbose=1, save_best_only=True)
 
-hist=  model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_valid, y_valid), callbacks=[checkpointer], verbose=2, shuffle=True)
+hist=  model.fit(datagen.flow(x_train, y_train, batch_size=32), epochs=10, validation_data=(x_valid, y_valid), callbacks=[checkpointer], verbose=2, shuffle=True)
 
 
 #val_acc가 가장 좋았던 가중치 사용하기
@@ -141,7 +164,7 @@ y_true = np.argmax(y_test, axis=1)
 # 레이블에 해당하는 이름
 class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-# 혼동 행렬
+# 혼동 행렬 
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -184,4 +207,7 @@ Test Accuracy:  0.6965000033378601
 
 2024-01-17 19:05
 Test Accuracy:  0.7813000082969666
+
+2024-01-18 05:13
+Test Accuracy:  0.739799976348877
 '''
--- README.md
+++ README.md
@@ -58,3 +58,34 @@ Test Accuracy: 0.7813000082969666 (2024-01-17 19:05)
 1. Parameter 개수 약 18만 개, 층을 더 많이 쌓아도 될 듯
 2. 개와 고양이 여전히 잘 구분 못 함
 3. 틀린 이미지를 보면 이미지 내에 불필요한 부분들이 존재. 전처리에도 신경을 써야할 듯함.
+
+#### 세 번째 모델
+
+전처리 - 픽셀값 정규화 + 데이터 증강(좌우 반전)
+
+| Layer | Output Shape  | Description |
+|-------|---------------|-------------|
+| Conv1  | 32 * 32 * 32  | Filters: (3 * 3 * 3) * 32 / Activation: ReLU|
+| Conv2 | 32 * 32 * 32 | Filters: (3 * 3 * 32) * 32 / Activation: ReLU|
+| MaxPool | 16 * 16 * 32 |            |
+| Conv3 | 16 * 16 * 64 | Filters: (3 * 3 * 32) * 64 / Activation: ReLU|
+| Conv4 | 16 * 16 * 64 | Filters: (3 * 3 * 64) * 64 / Activation: ReLU|
+| MaxPool | 8 * 8 * 64 |            |
+| Conv5 | 8 * 8 * 128 | Filters: (3 * 3 * 64) * 128 / Activation: ReLU|
+| Conv6 | 8 * 8 * 128 | Filters: (3 * 3 * 128) * 128 / Activation: ReLU|
+| Conv7 | 8 * 8 * 128 | Filters: (3 * 3 * 128) * 128 / Activation: ReLU|
+| MaxPool | 4 * 4 * 128 |            |
+| Dropout | 4 * 4 * 128   | 훈련 시 Layer의 20%의 뉴런을 무작위로 비활성화 | 
+| Flatten | 2028  |           |
+| FC1   |  500    | Activation: ReLU    |
+| Dropout |  500  | 훈련 시 Layer의 40%의 뉴런을 무작위로 비활성화 |
+| FC2   | 10     | Activation: SoftMax   | 
+
+Test Accuracy:  0.739799976348877 (2024-01-18 05:13)
+
+특이사항
+
+1. Layer를 확 늘리고 전처리 과정에 데이터 증강도 포함했으나 오히려 정확도가 떨어짐
+2. Parameter 개수 약 146만 개, 오히려 너무 Parameter 수가 많은 것이 문제일 수도
+3. 이전 모델에서 Dropout 비율과 FC Layer 수도 바꿨는데 이 부분이 문제일 수도
+4. 데이터 증강 과정에 줌인/줌아웃을 넣었다가 없앴는데 다시 넣어보는 게 나을지도
